80b77c9716beecc37d0a56d782cc34b34975c75d
Anthony Lopez <grungekarma@gmail.com>
2021-05-18 23:48:55 -0500

    make function

---
+++ components/Nav.js
@@ -0,0 +1,9 @@
+export default () => `
+<nav>
+  <i class="fas fa-bars"></i>
+  <ul class="hidden--mobile nav-links">
+    <li><a href="index.html">Home</a></li>
+    <li><a href="#bio">Bio</a></li>
+    <li><a href= "#gallery">Gallery</a></li>
+  </ul>
+</nav>`;
